46dd088d0f600aa19f7e47e8be1185e8066780e4
Kai Hu <kwrongkey@gmail.com>
2017-05-31 16:48:04 +0800

    [Kai] add html template.

--- webpack.config.js
+++ webpack.config.js
@@ -9,41 +9,23 @@ module.exports = {
 
 	entry: [
 		'react-hot-loader/patch',
-        // activate HMR for React
-        
 		'webpack-dev-server/client?http://localhost:3000',
-        // bundle the client for webpack-dev-server
-        // and connect to the provided endpoint
-		
 		'webpack/hot/only-dev-server',
-        // bundle the client for hot reloading
-        // only- means to only hot reload for successful updates
-		
 		'./index.js'
-        // the entry point of our app
 	],
+
 	output: {
 		filename: 'bundle.js',
-    // the output bundle
-
 		path: path.resolve(__dirname, 'dist'),
-
 		publicPath: '/'
-    // necessary for HMR to know where to load the hot update chunks
 	},
 
 	devtool: 'inline-source-map',
 
 	devServer: {
 		hot: true,
-    // enable HMR on the server
-
 		contentBase: path.resolve(__dirname, 'dist'),
-    // match the output path
-
 		publicPath: '/',
-    // match the output `publicPath`
-
 		port: 3000
 	},
 
@@ -59,6 +41,7 @@ module.exports = {
 			}
 		]
 	},
+	
 	plugins: [
 		new webpack.HotModuleReplacementPlugin(),
 		new webpack.NamedModulesPlugin(),
